5656d7ed0eb7bc44649286c85e99ca709c8d002c
EdileyGomes <edi.gomezzzva@gmail.com>
2025-09-15 14:42:16 -0400

    Aulão do guanabara

--- aula-06/desafio004.py
+++ aula-06/desafio004.py
@@ -1,2 +1,15 @@
-msg = input('Digite qualquer coisa: ')
-print(f'Oque você digitou foi {msg}, e ele pode ser {msg.isnumeric(), msg.isalpha(), msg.isalnum(), msg.isidentifier()}')
+def chamada():
+    msg = input('Digite qualquer coisa: ')
+
+    print(f'O tipo primitivo de {msg} é: {type(msg)}')
+    print(f'Só tem espaços?: {msg.isspace()}')
+    print(f'É um número?: {msg.isalnum()}')
+    print(f'É alfabético?: {msg.isalpha()}')
+    print(f'É alfanumérico?: {msg.isalnum()}')
+    print(f'Estão em maiusculas?: {msg.isupper()}')
+    print(f'Estão em minúsculas?: {msg.islower()}')
+    print(f'Estão capitalizadas?: {msg.istitle()}')
+    chamada()
+    if msg == 'desisto':
+        breakpoint()
+chamada()
