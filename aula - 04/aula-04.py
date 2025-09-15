5df7ce44db68646950765c1bcb8ad233adfb9430
EdileyGomes <edi.gomezzzva@gmail.com>
2025-09-15 09:31:18 -0400

    ...

--- aula - 04/aula-04.py
+++ aula - 04/aula-04.py
@@ -1,1 +1,22 @@
 print ('Olá, Mundo!')
+
+print(7 + 4)
+print('7'+'4')
+
+print('olá, Mundo!')
+nome = 'Ediley'
+idade = '19'
+peso =  63.4
+
+print(f'Seu nome é, {nome}, você tem {idade}, e tem {peso}Kg')
+print('-=*='* 20)
+
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+
+print('-=*='* 20)
+
+imc = peso / (altura * altura)
+print(f'Olá, {nome}! Você tem {idade} anos, sua altura é {altura}, e você pesa {peso}, logo seu IMC é {imc}')
---
+++ aula - 04/desafio03.py
@@ -0,0 +1,7 @@
+from uaclient.api.u.pro.attach.auto.full_auto_attach.v1 import event
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite Outro: '))
+
+print(f'a soma de {num1} + {num2} = {num1 + num2}')
+
