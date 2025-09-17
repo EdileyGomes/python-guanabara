e52f140fb4e77200edf30ee37d023e4e1826d232
EdileyGomes <edi.gomezzzva@gmail.com>
2025-09-16 20:29:24 -0400

    ...

---
+++ Aula-07/teste.py
@@ -0,0 +1,51 @@
+def inicio ():
+    n = float(input('Digite um número: '))
+    n2 = float(input('Digite outro número: '))
+
+    def alternativa():
+        alt = input('DESEJA CONTINUAR?\nDIGITE [S], PARA CONTINUAR;\nE DIGITE [N], PARA NÃO CONTINUAR!').upper()
+        if alt == 'N':
+            print('Paramos por aqui, Muito Obrigado, e volte sempre!')
+        elif alt == 'S':
+            print('OK, VAMOS CONTINUAR!!!')
+            inicio()
+        else:
+            print('Não consegui entender... Pode digitar dnv?')
+            alternativa()
+
+    def somar():
+        print(n + n2)
+
+    def dividir():
+        print(n / n2)
+
+    def subi():
+        print(n - n2)
+
+    def multiplicar():
+        print(n * n2)
+
+    opcao = input('Abaixo deixo as opções do que deve fazer, para trabalhar com os números acima!:\n'
+                  'SOMAR = +;\n'
+                  'SUBTRAIR = -;\n'
+                  'DIVIDIR = /;\n'
+                  'MULTIPLICAR = X.\n'
+                  'Digite sua escolha:').upper()
+
+    if opcao == '+':
+        somar()
+        alternativa()
+
+    elif opcao == '-':
+        subi()
+        alternativa()
+
+    elif opcao == 'X':
+        multiplicar()
+        alternativa()
+
+    else:
+        dividir()
+        alternativa()
+
+inicio()
